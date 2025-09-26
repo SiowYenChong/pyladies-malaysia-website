a8bea3cb1f9560f9e308bf438a8a5f6f83b5d953
SiowYenC <Clairechong998@gmail.com>
2025-09-26 09:33:54 +0800

    completed Site v1.0

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
-from home import views
+from theme import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-
-    path("__reload__/", include("django_browser_reload.urls")),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('events/', views.events, name='events'),
+    path('speakers/', views.speakers, name='speakers'),
+    path('articles/', views.articles, name='articles'),
+    path('faq/', views.faq, name='faq'),
+    path('becomeMember/', views.become_member, name='become_member'),
+    path('blog/', views.blog, name='blog'),
+    path('<slug:slug>/', views.blog_detail, name='blog_detail'),
 ]
 
 if settings.DEBUG:
--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
-from . import views
+from theme import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('events/', views.events, name='events'),
+    path('speakers/', views.speakers, name='speakers'),
+    path('articles/', views.articles, name='articles'),
+    path('faq/', views.faq, name='faq'),
+    path('becomeMember/', views.become_member, name='become_member'),
+    path('blog/', views.blog, name='blog'),
+    path('<slug:slug>/', views.blog_detail, name='blog_detail'),
 ]
 
